90a8cc9cc66cb0450f3067e615e773f5a5161610
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-02-26 15:04:30 -0500

    footer change
    changer le margin dans footer

---
+++ exercise 2 - gr1 - christopherStevens/exercise 2 - gr1 - christopherStevens/header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * L'entête « header » de chacune des pages de notre site
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head() ?>
+    <?php show_admin_bar(true); ?>
+</head>
+<body  <?php body_class("site"); ?>>
+<header class="site__header">
+    <h1 class="header__titre">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">    
+            <?php bloginfo('name'); ?>
+        </a>
+        
+    </h1>
+    <h2 class="header__description"><?php bloginfo('description'); ?></h2>
+    <div class="util">
+    <div class="util__icone">
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zm6 0a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zm5-1a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path></svg>
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd"></path></svg>
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path><path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd"></path></svg>
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000000"><path fill-rule="evenodd" d="M10.496 2.132a1 1 0 00-.992 0l-7 4A1 1 0 003 8v7a1 1 0 100 2h14a1 1 0 100-2V8a1 1 0 00.496-1.868l-7-4zM6 9a1 1 0 00-1 1v3a1 1 0 102 0v-3a1 1 0 00-1-1zm3 1a1 1 0 012 0v3a1 1 0 11-2 0v-3zm5-1a1 1 0 00-1 1v3a1 1 0 102 0v-3a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
+    </div>
+        <?php get_search_form() ?>  <!-- form recherche -->
+        
+    </div>
+</header>
+<section class="site__barre">
+    <input type="checkbox"  id="chk-burger">
+    <label for="chk-burger" id="burger">
+        <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#ddd"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </label>
+    <?php wp_nav_menu(array("menu"=>"principal",
+                            "container"=>"nav")); ?>
+</section>
---
+++ exercise 2 - gr1 - christopherStevens/exercise 2 - gr1 - christopherStevens/single-post.php
@@ -0,0 +1,19 @@
+<?php get_header() ?>
+<main class="site__main">
+     <h1>-------  single-post.php ----------</h1>
+     <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+          <article class="cours">
+               <?php the_post_thumbnail("medium"); ?>
+               <h1 class="cours__titre">
+                    <?php the_title(); ?>
+               </h1>    
+          <section>
+               <?php the_post_thumbnail("medium"); ?>
+               <?php echo get_the_content(); ?>
+          </section>
+               
+          </article>
+     <?php endwhile; ?>
+     <?php endif; ?>
+</main>
+<?php get_footer(); ?>
